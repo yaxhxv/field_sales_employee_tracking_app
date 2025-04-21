e930d8e5d90542f2938f0e68ba840321abae8095
yaxhxv <yashnawgiri45.com>
2025-04-21 11:53:49 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const connectDB = require('./config/db');
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js'; 
+ import cors from 'cors';
+
+dotenv.config();
 const app = express();
 
-require('dotenv').config();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API running');
+});
+
 connectDB();
 
-app.use(express.json()); // For body parsing
-app.get('/', (req, res) => res.send('API Running'));
-
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
